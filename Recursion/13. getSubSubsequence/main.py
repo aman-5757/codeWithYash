8573be20929180714f652e00684ad0a392aee35e
Aman Srivastava <aman4863@gmail.com>
2022-09-11 23:33:31 +0530

    added

---
+++ Recursion/13. getSubSubsequence/main.py
@@ -0,0 +1,21 @@
+str = "abc"
+
+def getSubsequence(str)->list:
+    if len(str) == 0:
+        baseAns = []
+        baseAns.append("")
+        return baseAns
+    
+    ch = str[0]
+    recAns = getSubsequence(str[1:])
+    myAns = []
+    for r in recAns:
+        myAns.append(r)
+
+    for r in recAns:
+        myAns.append(ch+r)
+
+    return myAns
+
+ans = getSubsequence(str)
+print(ans)
