f78c02bb77c96092c70aee0f16c4782777ae8851
Aman Srivastava <aman4863@gmail.com>
2022-09-12 21:52:28 +0530

    added

--- Recursion/10. printClimbStairs/main.py
+++ Recursion/10. printClimbStairs/main.py
@@ -12,4 +12,20 @@ def printClimbStairs(n, asf):
     if n-2 >= 0:
         printClimbStairs(n-2,asf+"2")
 
+def countClimbStairs(n):
+    if n == 0 :
+        return 1
+    
+    myAns = 0
+
+    if  n-1 >= 0:
+        myAns += countClimbStairs(n-1)
+    if n-2 >= 0:
+        myAns += countClimbStairs(n-2)
+
+    return myAns
+
 printClimbStairs(n,"")
+
+ans = countClimbStairs(n)
+print(ans)
